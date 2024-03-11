38a851fbfe83f5c2417b8854055fb6b28ec04d23
coelhohds <henriquecosantos04@gmail.com>
2024-03-11 14:44:50 -0300

    projeto inicial

---
+++ app.js
@@ -0,0 +1,26 @@
+function exibeTexto(tag, texto){
+    let teste = document.querySelector(tag);
+    teste.innerHTML = texto;
+}
+
+exibeTexto('h1', 'Hora do desafio')
+
+function consoleBotao(){
+    console.log('O botão foi clicado');
+}
+
+function alertaBotao(){
+    alert('Eu amo JS');
+}
+
+function promptBotao(){
+    let cidade = prompt('Escreva uma cidade do Brasil');
+    alert(`Estive em ${cidade} e lembrei de você`)
+}
+
+function somaBotao() {
+    let num1 = parseInt(prompt('Digite um número'));
+    let num2 = parseInt(prompt('Digite outro número'));
+    let resultado = num1 + num2
+    alert(`A soma entre ${num1} e ${num2} é ${resultado}`)
+}
